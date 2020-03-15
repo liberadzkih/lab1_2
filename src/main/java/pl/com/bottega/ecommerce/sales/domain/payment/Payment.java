a77635317ab140316f73c851448758011b975b11
AdamWojtczak <adamwojtczak807@gmail.com>
2020-03-15 20:04:54 +0100

    implementacja wzorca fabryki na klasach Invoice i Payment
    Signed-off-by: AdamWojtczak <adamwojtczak807@gmail.com>

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
@@ -24,15 +24,17 @@ public class Payment {
 
     private Id aggregateId;
 
-    public Payment(Id aggregateId, ClientData clientData, Money amount) {
+    private Payment(Id aggregateId, ClientData clientData, Money amount) {
         this.aggregateId = aggregateId;
         this.clientData = clientData;
         this.amount = amount;
     }
 
+    public Payment createPayment(Id aggregateId, ClientData clientData, Money amount) {
+        return new Payment(aggregateId, clientData, amount);
+    }
     public Payment rollBack() {
         Id id = Id.generate();
-
-        return new Payment(id, clientData, amount.multiplyBy(-1));
+        return createPayment(id, clientData, amount.multiplyBy(-1));
     }
 }
