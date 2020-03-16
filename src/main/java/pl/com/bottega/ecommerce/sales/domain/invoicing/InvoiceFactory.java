4e0c67d7451b8d15c57e423e79b3a9ce92ae0db8
debek.kacper <215702@edu.p.lodz.pl>
2020-03-16 15:23:29 +0100

    Added InvoiceFactory and implemented it in BookKeeper class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactory.java
@@ -0,0 +1,12 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+public class InvoiceFactory {
+
+    static Invoice createInvoice(Id id, ClientData client) {
+        return new Invoice(id, client);
+    }
+
+}
