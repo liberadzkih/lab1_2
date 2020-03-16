cbb9063315a8ba61254f3b309906e80e7956f0c6
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-03-16 17:41:29 +0100

    Change BookKeeper and Payment classes to use factory pattern

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
@@ -24,6 +24,8 @@ public class Payment {
 
     private Id aggregateId;
 
+    private PaymentFactory paymentFactory = new PaymentFactory();
+
     public Payment(Id aggregateId, ClientData clientData, Money amount) {
         this.aggregateId = aggregateId;
         this.clientData = clientData;
@@ -31,8 +33,6 @@ public class Payment {
     }
 
     public Payment rollBack() {
-        Id id = Id.generate();
-
-        return new Payment(id, clientData, amount.multiplyBy(-1));
+        return paymentFactory.getPayment(clientData, amount.multiplyBy(-1));
     }
 }
