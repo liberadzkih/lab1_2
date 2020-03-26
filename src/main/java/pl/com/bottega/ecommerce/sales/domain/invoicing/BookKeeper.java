f8df1f7f8ef2cb63d7fde71bd4c4104517cc1531
DGromek <dawidgromek.1997@gmail.com>
2020-03-26 13:11:30 +0100

    Added InvoiceFactory, refactored BookKeeper

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -19,10 +19,12 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory.createInvoice;
+
 public class BookKeeper {
 
     public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = new Invoice(Id.generate(), client);
+        Invoice invoice = createInvoice(Id.generate(), client);
 
         for (RequestItem item : items) {
             Money net = item.getTotalCost();
