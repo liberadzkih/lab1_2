769059f6c3a4211a0c4340844e20212f22222533
liberadzkih <liberadzkih@gmail.com>
2020-07-08 22:21:52 +0200

    Created InvoiceFactory

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactory.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+public class InvoiceFactory {
+
+    public static Invoice create(ClientData clientData) {
+        return new Invoice(Id.generate(), clientData);
+    }
+}
